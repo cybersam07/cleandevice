0d012bcb200dbae3533e7b743eca3830c5e28514
cybersam07 <110772605+cybersam07@users.noreply.github.com>
2023-08-31 01:26:32 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,26 @@
-const links = document.querySelectorAll('.link');
-const overlay = document.querySelector('.overlay');
-const modal = document.querySelector('.modal');
-const modalContent = document.querySelector('.modal-content');
-const modalCancelBtn = document.querySelector('.alt-btn button');
-const timeEl = document.querySelector('time');
+document.addEventListener("DOMContentLoaded", function () {
+    const ring = document.querySelector('.ring');
+    const approvedSign = document.querySelector('.approved-sign');
 
-links.forEach((link) => {
-    link.addEventListener('click', function (e) {
-        e.preventDefault();
-        overlay.style.display = 'block';
-        modal.style.display = 'block';
-        
-        // Pause the animations
-        document.querySelector('.ring').style.animationPlayState = 'paused';
-        document.querySelector('span').style.animationPlayState = 'paused';
+    ring.addEventListener('animationend', () => {
+        // Display the 'Click to Proceed' message
+        const clickToProceed = document.createElement('div');
+        clickToProceed.className = 'click-to-proceed';
+        clickToProceed.textContent = 'Click to Proceed';
+        document.body.appendChild(clickToProceed);
+
+        // Handle click event on the 'Click to Proceed' message
+        clickToProceed.addEventListener('click', () => {
+            // Remove the 'Click to Proceed' message
+            document.body.removeChild(clickToProceed);
+
+            // Display the approved sign
+            approvedSign.style.display = 'block';
+
+            // Close the page after a delay
+            setTimeout(() => {
+                window.close();
+            }, 2000); // 2000 milliseconds = 2 seconds
+        });
     });
 });
-
-modalCancelBtn.addEventListener('click', function () {
-    overlay.style.display = 'none';
-    modal.style.display = 'none';
-    
-    // Resume the animations
-    document.querySelector('.ring').style.animationPlayState = 'running';
-    document.querySelector('span').style.animationPlayState = 'running';
-});
-
-overlay.addEventListener('click', function (e) {
-    e.target.style.display = 'none';
-    modal.style.display = 'none';
-    
-    // Resume the animations
-    document.querySelector('.ring').style.animationPlayState = 'running';
-    document.querySelector('span').style.animationPlayState = 'running';
-});
-
-const currentDate = `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`;
-timeEl.innerText = currentDate;
-
-// Display the approved sign immediately and close the page
-const approvedSign = document.querySelector('.approved-sign');
-approvedSign.style.display = 'block';
-
-// Close the page after 2 seconds
-setTimeout(function () {
-    window.close();
-}, 2000);
